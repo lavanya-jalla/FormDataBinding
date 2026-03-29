c47c877edbe937e41e314148d865b8da556aaefd
Jalla Lavanya <lavanya123jalla@gmail.com>
2026-03-29 16:22:12 +0530

    your message

--- src/main/java/com/example/mvc/controller/StudentController.java
+++ src/main/java/com/example/mvc/controller/StudentController.java
@@ -1,24 +1,35 @@
 package com.example.mvc.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.mvc.model.Student;
+
 @Controller
 public class StudentController {
+	 @Value("${countries}")
+	    private List<String> countries;
+	 @Value("${languages}")
+	    private List<String> languages;
 	 @GetMapping("/showStudentForm")
 	    public String showForm(Model model) {
 		 model.addAttribute("student",new Student());
+		 model.addAttribute("countries",countries);
+		 model.addAttribute("languages",languages);
+
+		 model.addAttribute("osOptions", List.of("Windows", "Linux", "Mac"));
 	        return "student-form";  
 	    }
 
 	    @PostMapping("/processStudentForm")
 	    public String processForm(@ModelAttribute("student") Student student) {
-	    	System.out.println("Student: " + student.getFirstName() +" "+ student.getLastName());
+	    	System.out.println("Student: " + student.getFirstName() +" "+ student.getLastName() +" "+student.getCountry()+" "+student.getFavLang()+" "+ student.getFavOS());
 	        return "student-confirmation"; 
 	    }
 
--- src/main/java/com/example/mvc/model/Student.java
+++ src/main/java/com/example/mvc/model/Student.java
@@ -1,5 +1,7 @@
 package com.example.mvc.model;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,5 +12,8 @@ import lombok.NoArgsConstructor;
 public class Student {
 	private String firstName;
 	private String lastName;
+	private String country;
+	private String favLang;
+	private List<String> favOS;
 
 }
